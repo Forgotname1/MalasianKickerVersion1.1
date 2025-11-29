1f1df06c939fc0bcda348caf48cf41495b2b9348
MotorovIlia <107539219+Forgotname1@users.noreply.github.com>
2025-11-29 14:21:59 +0700

    Initial Commit

---
+++ MalaysionKicker_v1.1/MalaysionKicker_v1_1App.swift
@@ -0,0 +1,17 @@
+//
+//  MalaysionKicker_v1_1App.swift
+//  MalaysionKicker_v1.1
+//
+//  Created by Илья Моторов on 29/11/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct MalaysionKicker_v1_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
